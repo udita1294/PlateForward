ab3cd520cb11a5bfed6f37e506e6ff036bcb3e84
udita <aa.a@.com>
2025-09-20 14:56:36 +0530

    UserAuth added

--- Backend/Controller/UserController.js
+++ Backend/Controller/UserController.js
@@ -2,3 +2,71 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";   
 
+const generateToken = (user) => {
+    return jwt.sign(
+        { id: user._id, email: user.email, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+    );
+}
+
+const registerUser = async (req, res) => {
+    try {
+    const { name, email, password, role, phone, address } = req.body;
+    if(!name || !email || !password) {
+        return res.status(400).json({ message: "Missing details" });
+    }
+    const existingUser = await User.find({email});
+    if(existingUser){
+        return res.status(400).json({message: "User already exists"});
+    }
+
+    const salt =  await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({name,email,password:hashedPassword,role,phone,address});
+
+    res.status(201).json({
+        user:{id:user._id , name:user.name , email:user.email , role:user.role},
+        token : generateToken(user)
+    });
+
+    
+    } catch (error) {
+        console.error("Register error : ",error);
+        res.status(500).json({message: "Server Error"});
+    }
+}
+
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body; 
+        if(!email || !password) {
+            return res.status(400).json({ message: "Missing details" });
+        }
+        const user = await User.find({email});
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        res.status(200).json({
+            user:{id:user._id , name:user.name , email:user.email , role:user.role},
+            token : generateToken(user)
+        });
+
+
+    } catch (error) {
+        console.error("Login error : ",error);
+        res.status(500).json({message: "Server Error"});
+    }
+}
+
+const getUserProfile = async (req, res) => {
+    res.json({user: req.user});
+}
+
+export { registerUser, loginUser, getUserProfile };
+
