68e4f7fba6c56b5642ebd45bf894ff250b596086
udita <udita1294@gmail.com>
2025-11-19 23:54:34 +0530

    signup page edited

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -1,22 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 import axios from 'axios';
 
-
-export const StoreContext = createContext(null)
+export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const url = "http://localhost:3000";
+
+    const url = "http://localhost:3000/api/user"; 
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+        }
+    }, []);
 
     const contextValue = {
-        url
-        
-    }
+        url,
+        token,
+        setToken
+    };
 
     return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
-    )
-}
+    );
+};
 
-export default StoreContextProvider
+export default StoreContextProvider;
--- Frontend/src/Pages/signup.jsx
+++ Frontend/src/Pages/signup.jsx
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState ,useContext} from "react";
 import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const {url,setToken} = useContext(StoreContext);
+
   const [role, setRole] = useState("donor");
   const [form, setForm] = useState({
     name: "",
@@ -30,8 +33,15 @@ export default function Signup() {
       role,
     };
 
-    // await axios.post("http://localhost:3000/api/user/register", payload);
-    alert("Signup successful!");
+    const response = await axios.post(url,payload);
+
+      if (response.data.success) {
+        setToken(response.data.token);
+        localStorage.setItem("token",response.data.token);
+        // setShowLogin(false);
+      }else{
+        alert(response.data.message);
+      }
   };
 
   const roleCard = (value, title, subtitle) => (
@@ -39,8 +49,8 @@ export default function Signup() {
       onClick={() => setRole(value)}
       className={`cursor-pointer p-5 rounded-xl border ${
         role === value
-          ? "bg-[#e28c0a] border-[#d4c0a1] text-black"
-          : "bg-[#d4c0a1] border-[#4a3a31] text-black"
+          ? "bg-[#e28c0a] border-[#f5eadb] text-black"
+          : "bg-[#f5eadb] border-[#4a3a31] text-black"
       } transition-all`}
     >
       {/* <div className="text-3xl mb-2">🍽️</div> */}
@@ -52,14 +62,17 @@ export default function Signup() {
   return (
     <div className="min-h-screen w-full bg-gradient-to-b from-[#F9F9F6] to-[#f0fdf4] flex items-start justify-center pt-20 text-black">
       <div className="w-[750px] bg-[#cbf3cd] rounded-3xl p-10 shadow-lg border border-[#3a2f27]">
-        <h1 className="text-4xl text-green-700 font-bold text-center mb-3">Join FoodShare</h1>
+        <h1 className="text-4xl text-green-700 font-bold text-center mb-3">
+          Join FoodShare
+        </h1>
         <p className="text-center  mb-10">
           Create your profile to start making a difference in your community
         </p>
 
         <h2 className="text-lg font-semibold mb-2">Profile Setup</h2>
         <p className="text-sm opacity-70 mb-5">
-          Tell us about yourself so we can connect you with the right opportunities
+          Tell us about yourself so we can connect you with the right
+          opportunities
         </p>
 
         {/* Role Selection */}
@@ -77,7 +90,7 @@ export default function Signup() {
               <input
                 type="text"
                 name="name"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="Full Name"
                 onChange={handleChange}
               />
@@ -87,7 +100,7 @@ export default function Signup() {
               <input
                 type="text"
                 name="email"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="Email Address"
                 onChange={handleChange}
               />
@@ -96,7 +109,7 @@ export default function Signup() {
               <input
                 type="password"
                 name="password"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="Password"
                 onChange={handleChange}
               />
@@ -105,7 +118,7 @@ export default function Signup() {
               <input
                 type="text"
                 name="phone"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="Phone Number"
                 onChange={handleChange}
               />
@@ -119,7 +132,7 @@ export default function Signup() {
             <input
               type="text"
               name="street"
-              className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+              className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
               placeholder="Street Address"
               onChange={handleChange}
             />
@@ -131,7 +144,7 @@ export default function Signup() {
               <input
                 type="text"
                 name="city"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="City"
                 onChange={handleChange}
               />
@@ -141,7 +154,7 @@ export default function Signup() {
               <input
                 type="text"
                 name="state"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="State"
                 onChange={handleChange}
               />
@@ -151,23 +164,30 @@ export default function Signup() {
               <input
                 type="text"
                 name="zip"
-                className="w-full mt-1 p-3 rounded-lg bg-[#d4c0a1] border border-[#4a3a31]"
+                className="w-full mt-1 p-3 rounded-lg bg-[#f5eadb] border border-[#4a3a31]"
                 placeholder="Pin Code"
                 onChange={handleChange}
               />
             </div>
           </div>
+          <div className="flex gap-3">
+            <input type="checkbox" required />
+            <p className="font-semibold">
+              By continuing , I agree to the terms of use and privacy policy
+            </p>
+          </div>
+          <p className="font-semibold">
+            Already have an account ?{" "}
+            <Link to="/login">
+              <span className="text-orange-500">Login Here</span>
+            </Link>
+          </p>
           <button
             type="submit"
             className="cursor-pointer w-full mt-5 p-3 rounded-xl bg-[#ed6a07] text-black font-semibold hover:bg-[#a24804] transition-all"
           >
             Create Account
           </button>
-          <div>
-           <input type='checkbox' required/>
-          <p>By continuing , I agree to the terms of use and privacy policy</p>
-        </div>
-        <p>Already have an account ? <span>Login Here</span></p>
         </form>
       </div>
     </div>
