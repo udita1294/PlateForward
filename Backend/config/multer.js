85e081df41a2386cf0d84f6cea8e307e541db1fd
udita <udita1294@gmail.com>
2025-11-21 00:29:55 +0530

    multer.js added

---
+++ Backend/config/multer.js
@@ -0,0 +1,13 @@
+import multer from 'multer';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+import cloudinary from './cloudinary';
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params:{
+        folder: 'PlateForward',
+        allowed_formats: ['jpg', 'png', 'jpeg']
+    },
+});
+
+export const upload = multer({ storage: storage });
