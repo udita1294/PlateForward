73b7f1c1d38204728197c0dde7ed71b569be3e48
Udita Singh <udita1294@gmail.com>
2026-01-10 03:00:12 +0530

    changes made in socket.js

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 const socket = io(import.meta.env.VITE_API_URL || "https://plateforward.onrender.com", {
   transports: ['websocket']
 });
-export default socket;
+export default socket ;
