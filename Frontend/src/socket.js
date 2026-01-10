6f4cc78525e3b67fc8cef444ea6168a89376bcfb
Udita Singh <udita1294@gmail.com>
2026-01-10 11:47:51 +0530

    url corrected

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000" || "https://plateforward.onrender.com", {
+const socket = io( import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
+, {
   transports: ['websocket']
 });
 export default socket ;
