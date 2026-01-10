c5b76ff7ddefd523141a926774944a27e94f68a5
UDITA SINGH <udita1294@gmail.com>
2026-01-10 13:06:46 +0530

    Fix socket connection URL initialization

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io( "https://plateforward.onrender.com" || "http://localhost:3000"
+const socket = io( "https://plateforward.onrender.com"
 , {
   transports: ['websocket']
 });
